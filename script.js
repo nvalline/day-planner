0ae6f4438bc4bf9f67c0a90593399aa238e61534
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-03-27 19:51:35 -0600

    added rows to DOM

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const currentDate = moment().format('dddd, MMMM Do, YYYY')
-const workHours = ['9:00 AM'. '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM']
+const workHours = ['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM']
 
 // Get Current Date and Time
 $('#currentDay').append(currentDate)
 
 // On page load
 $('document').ready(function () {
-    printRows()
+    $.each(workHours, function (i, hour) {
+        const newRow = $('.container').append('<div class="row">')
+        const newHour = $('.row').append('<div class="hour">').text(hour)
+        const newInput = $('.row').append('<input type="text" class="task">')
+        const newButton = $('.row').append('<button class="saveBtn">')
+        const newSaveIcon = $('.saveBtn').append('<i class="far fa-save">')
+        // printRows(hour)
+        console.log(hour)
+    })
 })
 
 // Loop through workHours and print content
 
 
 // add row and elements to page
-function printRows() {
-    $('.container').append('<div class="row">')
-
-    $('.row').append('<div class="hour">', '<input type="text" class="task">', '<button class="saveBtn">')
-
-    $('.saveBtn').append('<i class="far fa-save">')
+function printRows(hour) {
 }
 
 $(".saveBtn").on('click', function () {
